a344804b57f8637e478f67e46650de70eb8b829d
LiamPSxd <zS20018158@estudiantes.uv.mx>
2023-11-09 09:51:27 -0600

    POJO - Categoria

---
+++ APICuponSmart/src/java/modelo/pojo/entidad/Categoria.java
@@ -0,0 +1,29 @@
+package modelo.pojo.entidad;
+
+public class Categoria{
+    private Integer id;
+    private String nombre;
+    
+    public Categoria(){}
+
+    public Categoria(Integer id, String nombre){
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public Integer getId(){
+        return id;
+    }
+
+    public void setId(Integer id){
+        this.id = id;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+}
