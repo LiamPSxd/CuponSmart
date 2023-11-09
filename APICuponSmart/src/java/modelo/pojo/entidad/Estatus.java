fc5d1ff737b85e9c4d25fdabe10438355ac07d9e
LiamPSxd <zS20018158@estudiantes.uv.mx>
2023-11-09 09:52:51 -0600

    POJO - Estatus

---
+++ APICuponSmart/src/java/modelo/pojo/entidad/Estatus.java
@@ -0,0 +1,29 @@
+package modelo.pojo.entidad;
+
+public class Estatus{
+    private Integer id;
+    private String estado;
+    
+    public Estatus(){}
+
+    public Estatus(Integer id, String estado){
+        this.id = id;
+        this.estado = estado;
+    }
+
+    public Integer getId(){
+        return id;
+    }
+
+    public void setId(Integer id){
+        this.id = id;
+    }
+
+    public String getEstado(){
+        return estado;
+    }
+
+    public void setEstado(String estado){
+        this.estado = estado;
+    }
+}
