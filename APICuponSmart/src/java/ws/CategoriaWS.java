d5eecc031293a56ea076417d9912c019c336811e
LiamPSxd <zS20018158@estudiantes.uv.mx>
2023-11-09 10:10:12 -0600

    WS - Categoria

---
+++ APICuponSmart/src/java/ws/CategoriaWS.java
@@ -0,0 +1,102 @@
+package ws;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
+import modelo.CategoriaDAO;
+import modelo.pojo.entidad.Categoria;
+import modelo.pojo.respuesta.RespuestaCategoria;
+import utils.Constantes;
+import utils.Verificaciones;
+
+@Path("categorias")
+public class CategoriaWS{
+    @Context
+    private UriInfo context;
+    
+    @GET
+    @Path("obtenerCategorias")
+    @Produces(MediaType.APPLICATION_JSON)
+    public RespuestaCategoria obtenerCategorias(){
+        return CategoriaDAO.obtenerCategorias();
+    }
+    
+    @GET
+    @Path("obtenerCategoriaPorId/{idCategoria}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public RespuestaCategoria obtenerCategoriaPorId(@PathParam("idCategoria") Integer idCategoria){
+        if(Verificaciones.Datos.numerico(idCategoria)){
+            return CategoriaDAO.obtenerCategoriaPorId(idCategoria);
+        }else{
+            return (RespuestaCategoria) Verificaciones.Excepciones.badRequest();
+        }
+    }
+    
+    @POST
+    @Path("registrar")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public RespuestaCategoria registrarCategoria(String jsonParam){
+        RespuestaCategoria respuesta = new RespuestaCategoria();
+        
+        try{
+            Gson gson = new Gson();
+            Categoria categoria = gson.fromJson(jsonParam, Categoria.class);
+            
+            if(Verificaciones.Datos.claseNoNula(categoria) && Verificaciones.Datos.cadena(categoria.getNombre())){
+                respuesta = CategoriaDAO.registrarCategoria(categoria);
+            }else{
+                return (RespuestaCategoria) Verificaciones.Excepciones.badRequest();
+            }
+        }catch(JsonSyntaxException e){
+            respuesta.setMensaje(Constantes.Excepciones.JSON_SYNTAX);
+        }
+        
+        return respuesta;
+    }
+    
+    @PUT
+    @Path("modificar")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public RespuestaCategoria modificarCategoria(String jsonParam){
+        RespuestaCategoria respuesta = new RespuestaCategoria();
+        
+        try{
+            Gson gson = new Gson();
+            Categoria categoria = gson.fromJson(jsonParam, Categoria.class);
+            
+            if(Verificaciones.Datos.claseNoNula(categoria) && Verificaciones.Datos.numerico(categoria.getId()))
+                respuesta = CategoriaDAO.modificarCategoria(categoria);
+            else{
+                return (RespuestaCategoria) Verificaciones.Excepciones.badRequest();
+            }
+        }catch(JsonSyntaxException e){
+            respuesta.setMensaje(Constantes.Excepciones.JSON_SYNTAX);
+        }
+        
+        return respuesta;
+    }
+    
+    @DELETE
+    @Path("eliminar")
+    @Produces(MediaType.APPLICATION_JSON)
+    public RespuestaCategoria eliminarCategoria(@FormParam("idCategoria") Integer idCategoria){
+        if(Verificaciones.Datos.numerico(idCategoria)){
+            return CategoriaDAO.eliminarCategoria(idCategoria);
+        }else{
+            return (RespuestaCategoria) Verificaciones.Excepciones.badRequest();
+        }
+    }
+}
