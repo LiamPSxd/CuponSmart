544b94409af54effc8b169a1f02d11e25e6cd692
LiamPSxd <zS20018158@estudiantes.uv.mx>
2023-11-09 09:53:53 -0600

    POJO - TipoPromocion

---
+++ APICuponSmart/src/java/modelo/pojo/entidad/TipoPromocion.java
@@ -0,0 +1,29 @@
+package modelo.pojo.entidad;
+
+public class TipoPromocion{
+    private Integer id;
+    private String tipo;
+    
+    public TipoPromocion(){}
+
+    public TipoPromocion(Integer id, String tipo){
+        this.id = id;
+        this.tipo = tipo;
+    }
+
+    public Integer getId(){
+        return id;
+    }
+
+    public void setId(Integer id){
+        this.id = id;
+    }
+
+    public String getTipo(){
+        return tipo;
+    }
+
+    public void setTipo(String tipo){
+        this.tipo = tipo;
+    }
+}
