ead8d15d72e83896f6dd62150527d363c10fa9ec
LiamPSxd <zS20018158@estudiantes.uv.mx>
2023-11-09 09:53:00 -0600

    POJO - Municipio

---
+++ APICuponSmart/src/java/modelo/pojo/entidad/Municipio.java
@@ -0,0 +1,29 @@
+package modelo.pojo.entidad;
+
+public class Municipio{
+    private Integer id;
+    private String nombre;
+    
+    public Municipio(){}
+
+    public Municipio(Integer id, String nombre){
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public Integer getId(){
+        return id;
+    }
+
+    public void setId(Integer id){
+        this.id = id;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+}
