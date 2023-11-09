1e84ab57ffe8b795152eb01ec87e6998b70b58b7
LiamPSxd <zS20018158@estudiantes.uv.mx>
2023-11-09 10:03:09 -0600

    MyBatis - Util

---
+++ APICuponSmart/src/java/mybatis/MyBatisUtil.java
@@ -0,0 +1,26 @@
+package mybatis;
+
+import java.io.IOException;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class MyBatisUtil{
+    public static final String RESOURCE = "mybatis/mybatis-config.xml";
+    public static final String ENVIRONMENT = "development";
+    
+    public static SqlSession getSession(){
+        SqlSession session = null;
+        
+        try{
+            session = new SqlSessionFactoryBuilder().build(
+                    Resources.getResourceAsReader(RESOURCE),
+                    ENVIRONMENT
+            ).openSession();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        
+        return session;
+    }
+}
