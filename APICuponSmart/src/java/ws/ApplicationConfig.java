ebac1da33d83c338f5b55a116990b6287e0b3bc1
CatBenderson <ameyalli030@gmail.com>
2023-11-13 19:54:24 -0600

    Pantallas Promocion y Administradores

--- APICuponSmart/src/java/ws/ApplicationConfig.java
+++ APICuponSmart/src/java/ws/ApplicationConfig.java
@@ -18,15 +18,11 @@ public class ApplicationConfig extends Application{
         resources.add(ws.CategoriaWS.class);
         resources.add(ws.CiudadWS.class);
         resources.add(ws.ClienteWS.class);
-<<<<<<< HEAD
         resources.add(ws.DireccionWS.class);
         resources.add(ws.EmpresaWS.class);
-        resources.add(ws.PromocionWS.class);
-        resources.add(ws.UsuarioWS.class);
-=======
         resources.add(ws.PromocionSucursalWS.class);
         resources.add(ws.PromocionWS.class);
         resources.add(ws.SucursalWS.class);
->>>>>>> b7265d0ef83a6c6627d6e638d6dd47259e6a0d36
+        resources.add(ws.UsuarioWS.class);
     }
 }
