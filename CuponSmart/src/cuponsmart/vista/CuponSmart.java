5e9a56713f969d3402d4d139458acb4390b9a7b2
LiamPSxd <liamibrahim_117@hotmail.com>
2023-11-30 18:38:45 -0600

    Vista

--- CuponSmart/src/cuponsmart/vista/CuponSmart.java
+++ CuponSmart/src/cuponsmart/vista/CuponSmart.java
@@ -5,15 +5,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import utils.Constantes;
 
-public class CuponSmart extends Application{    
+public class CuponSmart extends Application{
     @Override
     public void start(Stage stage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("FXMLInicioSesion.fxml"));
-        
+
         Scene scene = new Scene(root);
-        
         stage.setScene(scene);
+        stage.setTitle(Constantes.Pantallas.INICIO_SESION);
         stage.show();
     }
 
